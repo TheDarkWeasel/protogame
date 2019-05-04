31e6cc45fa4eee4050044b12e0956a78bc966da2
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2019-05-04 14:36:49 +0200

    Added controller for placing objects via mouse

---
+++ BloodBuilder/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    private PlacementController mPlacementController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject playerBase = Resources.Load<GameObject>("Models/PlayerBase");
+        mPlacementController = new PlacementController(playerBase, KeyCode.A);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mPlacementController != null)
+        {
+            mPlacementController.Update();
+        }
+    }
+}
---
+++ BloodBuilder/Assets/Scripts/PlacementController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacementController
+{
+    [SerializeField]
+    private GameObject placeableObjectPrefab;
+
+    [SerializeField]
+    private KeyCode newObjectHotkey = KeyCode.A;
+
+    private GameObject currentPlaceableObject;
+    Vector3 specificVector = new Vector3();
+
+    public PlacementController(GameObject prefabToPlace, KeyCode hotkey)
+    {
+        this.placeableObjectPrefab = prefabToPlace;
+        this.newObjectHotkey = hotkey;
+    }
+
+    public void Update()
+    {
+        HandleNewObjectHotkey();
+
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentObjectToMouse();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void HandleNewObjectHotkey()
+    {
+        if (Input.GetKeyDown(newObjectHotkey))
+        {
+            if (currentPlaceableObject != null)
+            {
+                Object.Destroy(currentPlaceableObject);
+            }
+            else
+            {
+                currentPlaceableObject = Object.Instantiate(placeableObjectPrefab);
+            }
+        }
+    }
+
+    private void MoveCurrentObjectToMouse()
+    {
+        if(HasMouseMoved())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                specificVector.Set(hitInfo.point.x, hitInfo.collider.transform.position.y, hitInfo.point.z);
+                currentPlaceableObject.transform.position = specificVector;
+                Debug.Log("Point: " + currentPlaceableObject.transform.position);
+            }
+        }
+    }
+
+    private bool HasMouseMoved()
+    {
+        return (Input.GetAxis("Mouse X") != 0) || (Input.GetAxis("Mouse Y") != 0);
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;
+        }
+    }
+}
