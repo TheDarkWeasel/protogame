dba3f5aa1033d66eebf1baaa95b984ed32ad2c32
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2019-05-05 14:08:45 +0200

    Added support for closed source theme

---
+++ BloodBuilder/Assets/Scripts/Themes/ToonTheme.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToonTheme : Theme
+{
+    public string getPlayerBasePrefabPath()
+    {
+        return "ToonyTinyPeople/TT_RTS/TT_RTS_Standard/models/buildings/Castle";
+    }
+}
