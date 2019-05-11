3eeae20e9aa20e95e944a8b8d1ccc565ec7f5f9c
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2019-05-11 23:20:52 +0200

    Created infantry prefab

--- BloodBuilder/Assets/Scripts/Themes/OpenSourceTheme.cs
+++ BloodBuilder/Assets/Scripts/Themes/OpenSourceTheme.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class OpenSourceTheme : Theme
 {
+    public string getInfantryPrefabPath()
+    {
+        return "Models/Infantry";
+    }
+
     string Theme.getPlayerBasePrefabPath()
     {
         return "Models/PlayerBase";
--- BloodBuilder/Assets/Scripts/Themes/ToonTheme.cs
+++ BloodBuilder/Assets/Scripts/Themes/ToonTheme.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class ToonTheme : Theme
 {
+    public string getInfantryPrefabPath()
+    {
+        return "ToonyTinyPeople/TT_RTS/TT_RTS_Standard/prefabs/TT_Light_Infantry";
+    }
+
     public string getPlayerBasePrefabPath()
     {
         return "ToonyTinyPeople/TT_RTS/TT_RTS_Standard/models/buildings/Castle";
