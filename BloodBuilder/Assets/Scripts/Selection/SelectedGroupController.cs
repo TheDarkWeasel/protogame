dff77d743dde180d3e9860eb9667646a98663c38
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2019-08-15 16:02:51 +0200

    Added visual clue to show where the units will be moving to + improved unit movement when the target point lies within the current formation

--- BloodBuilder/Assets/Scripts/Selection/SelectedGroupController.cs
+++ BloodBuilder/Assets/Scripts/Selection/SelectedGroupController.cs
@@ -10,11 +10,15 @@ public class SelectedGroupController
     private bool isActive = false;
     private Camera mainCamera;
     private RaycastHit hitInfo;
+    private Vector3 center = new Vector3(0, 0, 0);
+
+    private GameObject selectionCirclePrefab;
 
     public SelectedGroupController(ContextProvider context)
     {
         this.context = context;
         mainCamera = Camera.main;
+        selectionCirclePrefab = Resources.Load<GameObject>(GameController.GetGlobalTheme().GetSelectionCirclePrefabPath());
     }
 
     public void Update()
@@ -37,7 +41,7 @@ public class SelectedGroupController
                 if (Physics.Raycast(ray, out hitInfo))
                 {
                     //Calculate the center of the currently selected units
-                    Vector3 center = new Vector3(0, 0, 0);
+                    center.Set(0, 0, 0);
 
                     foreach (IPlayerSelectableObject playerSelectableObject in selectedObjects)
                     {
@@ -46,15 +50,50 @@ public class SelectedGroupController
 
                     center /= selectedObjects.Count;
 
-                    //Move the selected units, respecting their current offset from the center of the group (so they stay in the same formation)
+                    float maxDistanceToCenter = 0f;
+
                     foreach (IPlayerSelectableObject playerSelectableObject in selectedObjects)
                     {
-                        Vector3 offset = playerSelectableObject.GetGameObject().transform.position - center;
-                        playerSelectableObject.OnSecondaryAction(hitInfo.point + offset);
+                        maxDistanceToCenter = Mathf.Max(Vector3.Distance(center, playerSelectableObject.GetGameObject().transform.position), maxDistanceToCenter);
+                    }
+
+                    if (Vector3.Distance(hitInfo.point, center) > maxDistanceToCenter)
+                    {
+                        //If the click is outside the current formation,
+                        //move the selected units, respecting their current offset from the center of the group (so they stay in the same formation)
+                        foreach (IPlayerSelectableObject playerSelectableObject in selectedObjects)
+                        {
+                            Vector3 offset = playerSelectableObject.GetGameObject().transform.position - center;
+                            playerSelectableObject.OnSecondaryAction(hitInfo.point + offset);
+                            ShowTargetClue(offset);
+                        }
+                    }
+                    else
+                    {
+                        //If the click is inside the current formation, just move the units to the point (with a small random offset).
+                        foreach (IPlayerSelectableObject playerSelectableObject in selectedObjects)
+                        {
+                            playerSelectableObject.OnSecondaryAction(hitInfo.point);
+                            ShowTargetClue(new Vector3(Random.Range(0, 1.5f), Random.Range(0, 1.5f), Random.Range(0, 1.5f)));
+                        }
                     }
                 }
             }
         }
+    }
+
+    /**
+     * Creates a hint to where the units will move to.
+     **/
+    private void ShowTargetClue(Vector3 offset)
+    {
+        //TODO Add pooling for this
+        GameObject targetClue = Object.Instantiate(selectionCirclePrefab);
+        targetClue.transform.position = hitInfo.point + offset;
+        targetClue.transform.eulerAngles = new Vector3(90, 0, 0);
+        targetClue.GetComponent<Projector>().orthographicSize = 1f;
+        targetClue.GetComponent<Animation>().Play("SelectionFadeOut");
+        Object.Destroy(targetClue, 1f);
     }
 
     public bool IsActive()
